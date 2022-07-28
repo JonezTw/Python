7de34c55136f4f7399b0050699447c107f8b20b1
JonezTw <joadossa@cisco.com>
2022-07-28 15:24:21 -0300

    third commit

---
+++ ex2.py
@@ -0,0 +1,4 @@
+m = int(input('Digite um valor em Metros: '))
+cm = m*100
+
+print(m,'m =',cm,'cm')
---
+++ ex3.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite a sua primeira nota: '))
+n2 = int(input('Digite a sua segunda nota: '))
+
+media = (n1+n2)/2
+print('sua média é igual a:',media)
